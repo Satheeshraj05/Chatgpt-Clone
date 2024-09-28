56998eaebb5176b52ed5dd096e58a50ffd00bff2
satheeshraj05 <satheeshraj333@gmail.com>
2024-09-28 21:38:57 +0530

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     env: {
-        NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-        NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+        NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '',
     },
-}
+};
 
-module.exports = nextConfig
+// Exporting the configuration
+export default nextConfig;
