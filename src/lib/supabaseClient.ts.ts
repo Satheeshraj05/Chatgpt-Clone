6ce06e9d9e9a36be8cfc172f944f634e34f38c18
satheeshraj05 <satheeshraj333@gmail.com>
2024-09-28 21:17:29 +0530

    supabase key

--- src/lib/supabaseClient.ts.ts
+++ src/lib/supabaseClient.ts.ts
@@ -1,15 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-
-console.log('Supabase URL:', supabaseUrl)
-console.log('Supabase Anon Key:', supabaseAnonKey)
-
-if (!supabaseUrl || !supabaseAnonKey) {
-    throw new Error(`Missing Supabase environment variables. 
-    NEXT_PUBLIC_SUPABASE_URL: ${supabaseUrl ? 'Set' : 'Not Set'}
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: ${supabaseAnonKey ? 'Set' : 'Not Set'}`)
-}
+const supabaseUrl = 'https://mbtdsosqfwgbjkckfnzj.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1idGRzb3NxZndnYmprY2tmbnpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc0NTI2NjQsImV4cCI6MjA0MzAyODY2NH0.02yL1hKnSOwvhg-DxlPo24hQk1qf2gCBOemRFAUdN3M'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
